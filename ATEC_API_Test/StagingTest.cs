d0f56bdfcf3f24d4f612d8bdd797594e4d6aeb51
russelake@gmail.com <russelake@gmail.com>
2024-05-07 21:08:06 +0800

    Add staga test

---
+++ ATEC_API_Test/StagingTest.cs
@@ -0,0 +1,11 @@
+namespace ATEC_API_Test
+{
+    public class StagingTest
+    {
+        [Fact]
+        public void Check_If_CurrentStage_Is_Already_TrackedOut()
+        {
+            Assert.True(true);
+        }
+    }
+}
