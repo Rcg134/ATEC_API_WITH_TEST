0c9dd47eb48c3cdaf3c1e11901afa716465fb0b6
russelake@gmail.com <russelake@gmail.com>
2024-05-15 17:57:21 +0800

    Add more integration test

--- ATEC_API_Test/StagingIntegrationTest/StagingAPITest.cs
+++ ATEC_API_Test/StagingIntegrationTest/StagingAPITest.cs
@@ -1,17 +1,21 @@
 
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.AspNetCore.Mvc.Testing;
 using System;
+using System.Text.Json;
 
 
 namespace ATEC_API_Test.StagingIntegrationTest
 {
     public class StagingAPITest : IClassFixture<WebApplicationFactory<Program>>
     {
-        private readonly WebApplicationFactory<Program> _factory;
+        private readonly HttpClient _client;
+
 
         public StagingAPITest(WebApplicationFactory<Program> factory)
         {
-            _factory = factory;
+            factory.ClientOptions.BaseAddress = new Uri("http://localhost/api/Staging/IsTrackOut");
+            _client = factory.CreateClient();
         }
 
 
@@ -21,14 +25,53 @@ namespace ATEC_API_Test.StagingIntegrationTest
         public async Task IStagingRepository_IsTrackOut_MustReturn_StatusCode(string LotAlias,
                                                                               int StatusCode)
         {
-            var client = _factory.CreateDefaultClient();
-            client.DefaultRequestHeaders.Add("paramLotAlias", LotAlias);
+            _client.DefaultRequestHeaders.Add("paramLotAlias", LotAlias);
+            var response = await _client.GetAsync("");
 
-            var response = await client.GetAsync("/api/Staging/IsTrackOut");
             Assert.Equal(StatusCode, (int)response.StatusCode);
         }
 
+        [Fact]
+        public async Task IStagingRepository_IsTrackOut_ReturnExpectedMediaType()
+        {
 
+            var response = await _client.GetAsync("");
+            _client.DefaultRequestHeaders.Add("paramLotAlias", "DUMMYLOT.1-A");
+
+            Assert.Equal("application/json", response.Content.Headers.ContentType.MediaType);
+        }
+
+        [Fact]
+        public async Task IStagingRepository_IsTrackOut_ReturnMustNotNull()
+        {
+            var response = await _client.GetAsync("");
+            _client.DefaultRequestHeaders.Add("paramLotAlias", "DUMMYLOT.1-A");
+
+            Assert.NotNull(response);
+            Assert.True(response.Content.Headers.ContentLength > 0);
+        }
+
+
+        [Fact]
+        public async Task IStagingRepository_IsTrackOut_ReturnsExpectedJSON()
+        {
+            var expectedJson = "{\"details\":{\"hasSetUp\":true,\"isTrackout\":true}}";
+
+            _client.DefaultRequestHeaders.Add("paramLotAlias", "DUMMYLOT.1-A");
+            var responseStream = await _client.GetStreamAsync("");
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            var responseModel = await JsonSerializer.DeserializeAsync<expectedIsTrackOutModel>(responseStream, options);
+
+            var actualJson = JsonSerializer.Serialize(responseModel);
+
+            Assert.NotNull(actualJson);
+            Assert.Equal(expectedJson, actualJson);
+        }
 
     }
 }
