757690358e4ed8e087290291646050445dc791a6
TheReal08 <140867039+TheReal08@users.noreply.github.com>
2024-09-11 16:26:44 +0800

    Update
    Update

--- Data/DTO/StagingDTO/MaterialStagingLMDTO.cs
+++ Data/DTO/StagingDTO/MaterialStagingLMDTO.cs
@@ -2,11 +2,11 @@
 {
     public class MaterialStagingLMDTO
     {
-        public string LotAlias { get; set; }
-        public string MachineNo { get; set; }
+        public string? LotAlias { get; set; }
+        public string? MachineNo { get; set; }
         public int Mode { get; set; }
-        public string Sid { get; set; }
-        public string MaterialId { get; set; }
-        public string Serial {  get; set; }
+        public string? Sid { get; set; }
+        public string? MaterialId { get; set; }
+        public string? Serial {  get; set; }
     }
 }
