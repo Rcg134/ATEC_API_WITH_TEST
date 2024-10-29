bccf6e3fbd8e40de0037b353576655c083a1f9cb
TheReal08 <140867039+TheReal08@users.noreply.github.com>
2024-10-29 13:48:44 +0800

    remove old repo

--- Data/DTO/LoginDTO/LoginDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ATEC_API.Data.DTO.LoginDTO
-{
-    public class LoginDTO
-    {
-        public string Username { get; set; }
-
-        public string Password { get; set; }
-    }
-}
--- GeneralModels/MESATECModels/StagingResponse/MaterialCustomerResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ATEC_API.GeneralModels.MESATECModels.StagingResponse
-{
-    public class MaterialCustomerResponse
-    {
-        public int? CustomerCode { get; set; }
-        public string? CustomerId { get; set; }
-    }
-}
